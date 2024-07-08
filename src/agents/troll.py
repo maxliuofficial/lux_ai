178048b17df3c34fe2da37ea7c9c484b8e4abf04
maxliuofficial <liumax2000@gmail.com>
2024-07-07 19:10:05 -0700

    some refactors

--- src/agents/troll.py
+++ src/agents/troll.py
@@ -1,15 +1,20 @@
-import math, sys
-from lux.game import Game
-from lux.game_map import Cell, RESOURCE_TYPES
-from lux.constants import Constants
-from lux.game_constants import GAME_CONSTANTS
+import math
+import sys
+
+from kaggle_environments.helpers import Configuration, Observation
+
 from lux import annotate
+from lux.constants import Constants
+from lux.game import Game
+from lux.game_constants import GAME_CONSTANTS
+from lux.game_map import RESOURCE_TYPES, Cell
+from lux.game_objects import Player
 
 DIRECTIONS = Constants.DIRECTIONS
 game_state = None
 
 
-def agent(observation, configuration):
+def agent(observation: Observation, configuration: Configuration):
     global game_state
 
     ### Do not edit ###
@@ -20,20 +25,23 @@ def agent(observation, configuration):
         game_state.id = observation.player
     else:
         game_state._update(observation["updates"])
-    
+
     actions = []
 
-    ### AI Code goes down here! ### 
-    player = game_state.players[observation.player]
-    opponent = game_state.players[(observation.player + 1) % 2]
+    ### AI Code goes down here! ###
+    player: Player = game_state.players[observation.player]
+    opponent: Player = game_state.players[(observation.player + 1) % 2]
     width, height = game_state.map.width, game_state.map.height
 
-    resource_tiles: list[Cell] = []
+    coal_targets: list[Cell] = []
     for y in range(height):
         for x in range(width):
             cell = game_state.map.get_cell(x, y)
-            if cell.has_resource():
-                resource_tiles.append(cell)
+            if (
+                cell.has_resource()
+                and cell.resource.type == Constants.RESOURCE_TYPES.COAL
+            ):
+                coal_targets.append(cell)
 
     # we iterate over all our units and do something with them
     for unit in player.units:
@@ -41,18 +49,30 @@ def agent(observation, configuration):
             closest_dist = math.inf
             closest_resource_tile = None
             if unit.get_cargo_space_left() > 0:
-                # if the unit is a worker and we have space in cargo, lets find the nearest resource tile and try to mine it
-                for resource_tile in resource_tiles:
-                    if resource_tile.resource.type == Constants.RESOURCE_TYPES.COAL and not player.researched_coal(): continue
-                    if resource_tile.resource.type == Constants.RESOURCE_TYPES.URANIUM and not player.researched_uranium(): continue
+                # If the unit is a worker and we have space in cargo, lets find the nearest
+                # resource tile and try to mine it
+                for resource_tile in coal_targets:
+                    if (
+                        resource_tile.resource.type == Constants.RESOURCE_TYPES.COAL
+                        and not player.researched_coal()
+                    ):
+                        continue
+                    if (
+                        resource_tile.resource.type == Constants.RESOURCE_TYPES.URANIUM
+                        and not player.researched_uranium()
+                    ):
+                        continue
                     dist = resource_tile.pos.distance_to(unit.pos)
                     if dist < closest_dist:
                         closest_dist = dist
                         closest_resource_tile = resource_tile
                 if closest_resource_tile is not None:
-                    actions.append(unit.move(unit.pos.direction_to(closest_resource_tile.pos)))
+                    actions.append(
+                        unit.move(unit.pos.direction_to(closest_resource_tile.pos))
+                    )
             else:
-                # if unit is a worker and there is no cargo space left, and we have cities, lets return to them
+                # If unit is a worker and there is no cargo space left, and we have cities,
+                # lets return to them
                 if len(player.cities) > 0:
                     closest_dist = math.inf
                     closest_city_tile = None
@@ -68,5 +88,5 @@ def agent(observation, configuration):
 
     # you can add debug annotations using the functions in the annotate object
     # actions.append(annotate.circle(0, 0))
-    
+
     return actions
